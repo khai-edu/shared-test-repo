d5501c2a51de96c48a8771788b54fcd8c64d1393
Chertishcheva <k.i.chertishcheva@student.khai.edu>
2023-11-20 14:47:47 +0200

    branch update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,8 @@ public class Main
 	System.out.println("Перший комміт в гілку develop!");
 	System.out.println("Другий комміт в гілку develop!");
 	System.out.println("Другий комміт в гілку main!");
-	System.out.println("Треітй комміт в гілку main!");
+	System.out.println("Третій комміт в гілку main!");
 	System.out.println("Початок роботи:");
+	System.out.println("Chertishcheva Kseniia");
     }
 }
